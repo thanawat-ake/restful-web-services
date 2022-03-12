67423a7cff901a70c8b1e8e49b3fd9186a8d0b32
Thanawat Techakasemsuk <thanawat@codapayments.com>
2022-03-13 02:37:00 +0700

    49. Step 33 - Creating Post Entity and Many to One Relationship with User Entity.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,6 +1,5 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import com.in28minutes.rest.webservices.restfulwebservices.post.Post;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -14,12 +13,8 @@ import java.util.List;
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
     private static int usersCount = 3;
-    private static List<Post> posts = new ArrayList<>();
 
     static {
-        posts.add(new Post(1,"I am happy today."));
-        posts.add(new Post(2,"Today is my birthday."));
-        posts.add(new Post(3,"Tomorrow is Saturday."));
         users.add(new User(1, "Adam", new Date()));
         users.add(new User(2, "Eve", new Date()));
         users.add(new User(3, "Jack", new Date()));
